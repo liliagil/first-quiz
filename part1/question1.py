f7572706aa1f10fbd1b4f9a8a13f80c443c588c1
liliagil <149601167+liliagil@users.noreply.github.com>
2023-11-03 19:03:13 -0500

    Update question1.py

--- part1/question1.py
+++ part1/question1.py
@@ -20,7 +20,8 @@ def get_city_temperature(city):
       return 17
    if city == "San Francisco":
       return 16
-
+ if city == "New York":
+      return 14
 def get_city_weather(city):
 
   sky_condition = None
@@ -29,7 +30,9 @@ def get_city_weather(city):
      sky_condition = "cloudy"
   elif city == "New York":
      sky_condition = "rainy"
-
+  if city== "Quito":
+     sky_condition="sunny"
+  
   temperature = get_city_temperature(city)
 
   return str(temperature) + " degrees and " + sky_condition
